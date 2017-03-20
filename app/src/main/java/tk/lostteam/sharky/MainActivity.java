99c7da3eec54401167cae854b3dc6b9b7f8d3bca
MCUnderground <daniel.barinak@gmail.com>
2017-03-20 23:02:45 +0100

    New look, and new stuff!

--- app/src/main/java/tk/lostteam/sharky/MainActivity.java
+++ app/src/main/java/tk/lostteam/sharky/MainActivity.java
@@ -1,23 +1,31 @@
 package tk.lostteam.sharky;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.*;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.*;
 
 
 public class MainActivity extends Activity {
 
     WebView webview;
+
     Button searchBtn;
+
+    ImageView backBtn;
+    ImageView menuBtn;
+
     EditText urlView;
+
     private SwipeRefreshLayout swipeLayout;
 
 
@@ -27,14 +35,22 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         webview = (WebView) findViewById(R.id.webview);
+
         searchBtn = (Button) findViewById(R.id.goBtn);
+
+        backBtn = (ImageView) findViewById(R.id.backBtn);
+        menuBtn = (ImageView) findViewById(R.id.popupMenuBtn);
+
         urlView = (EditText) findViewById(R.id.urlView);
 
         urlView.setFocusable(true);
         urlView.setSelectAllOnFocus(true);
 
+
+
         webview.setWebViewClient(new WebViewClient()
         {
+            
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 urlView.setText(url);
@@ -46,11 +62,18 @@ public class MainActivity extends Activity {
                 swipeLayout.setRefreshing(false);
             }
 
+
+            @SuppressWarnings("deprecation")
             @Override
-            public void onReceivedError(WebView viewer, WebResourceRequest request, WebResourceError error) {
-                viewer.loadUrl("file:///android_asset/error.html");
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                view.loadUrl("file:///android_asset/error.html");
             }
 
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                view.loadUrl("file:///android_asset/error.html");
+            }
         });
 
 
@@ -78,6 +101,46 @@ public class MainActivity extends Activity {
                 }
             }
 
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(webview.canGoBack()){
+                    webview.goBack();
+                } else {
+
+                }
+            }
+        });
+
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this, menuBtn);
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+                popupMenu.show();
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.home_menu:
+                                webview.loadUrl("http://google.com");
+                                return true;
+                            case R.id.settings_menu:
+                                Intent i_menu = new Intent(MainActivity.this, SettingsActivity.class);
+                                startActivity(i_menu);
+                                return true;
+                            case R.id.exit_menu:
+                                finish();
+                                return true;
+
+                        }
+                        return true;
+                    }
+                });
+            }
         });
 
         swipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe);
--- app/src/main/java/tk/lostteam/sharky/SettingsActivity.java
+++ app/src/main/java/tk/lostteam/sharky/SettingsActivity.java
@@ -35,4 +35,9 @@ public class SettingsActivity extends MainActivity{
         });
 
     }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
 }
